10b5d61670764a3b541f0a21098722bf5884c26a
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2023-07-25 10:24:25 +0600

    Update dfsBFS.js

--- dfsBFS.js
+++ dfsBFS.js
@@ -6,12 +6,20 @@ let grid=[]
 let resolution=30
 
 let rows,cols
+let isMousePressedbyme=false
 
 let strPoint=[]
 let endPoint=[]
 let newGrid=[]
+let r=0
+let g=206
+let b=209
+let paths=[]
 
-let found=false
+let found;
+let path;
+
+let rgba=["rgba(0, 0, 66, 0.75)","rgba(17, 104, 217, 0.75)","rgba(0, 217, 159, 0.75)","rgba(0, 190, 218, 0.75)"]
 
 
 
@@ -24,6 +32,7 @@ function setup(){
 
     rows=floor(height/resolution)
     cols=floor(width/resolution)
+    path=new Path()
  
 
     grid=make2dArray(rows,cols)
@@ -42,22 +51,67 @@ function setup(){
     button = createButton('BFS');
     button.position(0, 20);
     button.mousePressed(runBFS);
+    button = createButton('Clear');
+    button.position(0, 40);
+    button.mousePressed(clearBoard);
 
-    function runBG(){
-        dfs(strPoint[0],strPoint[1],grid)
+
+    function clearBoard(){
+        grid=make2dArray(rows,cols)
+        strPoint=[floor(random(rows)),floor(random(cols))]
+        endPoint=[floor(random(rows)),floor(random(cols))]
+        
+        grid[strPoint[0]][strPoint[1]].setSEpoint(true,false)
+        grid[endPoint[0]][endPoint[1]].setSEpoint(false,true)
     }
 
-    function runBFS(){
+    async function runBG(){
+         
+         dfs(strPoint[0],strPoint[1],grid,1,path)
+        console.log(paths)
+
+        setTimeout(()=>{
+            dfs2(strPoint[0],strPoint[1],grid,2,1)
+
+        },100)
+        setTimeout(()=>{
+            dfs2(strPoint[0],strPoint[1],grid,3,2)
+
+        },500)
+        setTimeout(()=>{
+            dfs2(strPoint[0],strPoint[1],grid,4,3)
+
+        },900)
+
+
+    }
+
+    async function runBFS(){
         BFS(strPoint[0],strPoint[1],grid)
+
+        setTimeout( ()=>{
+             BFS2(strPoint[0],strPoint[1],grid,1,2)
+         
+
+         },400)
+        //  setTimeout(()=>{
+        //     BFS2(strPoint[0],strPoint[1],grid,2)
+
+        //  },200)
+        setTimeout(()=>{
+            BFS2(strPoint[0],strPoint[1],grid,2,3)
+
+         },1000)
+        setTimeout(()=>{
+            BFS2(strPoint[0],strPoint[1],grid,3,4)
+
+         },1600)
+        
  
     }  
+    found=new Found()
     
    
-
-
-        
-    
-
 
 
     // for(let i=0;i<rows;i++){
@@ -76,8 +130,113 @@ function setup(){
 
 
 
+function windowResized() {
+    resizeCanvas(windowWidth-20, windowHeight-200);
+    
+    
+  }
+
+function mouseClicked(){
+
+    console.log(!isMousePressedbyme)
+
+if(!isMousePressedbyme){
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+
+            grid[i][j].mouseClicked(mouseX,mouseY)
+
+
+
+
+        }}
+
+}
+   
+
+}
+
+function mouseDragged(){
+    if(!isMousePressedbyme){
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+
+            grid[i][j].mouseClicked(mouseX,mouseY)
+
+
+
+
+        }}
+    }
+
+}
+
+
+
+
+function mousePressed(){
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+
+            if(grid[i][j].mouseClicked2(mouseX,mouseY)){
+                grid[i][j].strPoint=false
+                strPoint=null
+                isMousePressedbyme=true
+
+            }
+            if(grid[i][j].mouseClicked4(mouseX,mouseY)){
+                grid[i][j].endPoint=false
+                endPoint=null
+                isMousePressedbyme=true
+
+            }
+        }}
+    
+
+
+
+}
+function mouseReleased(){
+    if(isMousePressedbyme){
+     console.log(mouseX)
+     for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+
+            if(!strPoint){
+                grid[i][j].mouseClicked3(mouseX,mouseY)
+                // grid[i][j].strPoint=false
+
+            }
+            if(!endPoint){
+                grid[i][j].mouseClicked5(mouseX,mouseY)
+            }
+
+           
+                
+
+            
+        }
+
+     isMousePressedbyme=false
+
+    }
+    
+
+
+
+}
+}
+
+
+
+
 
 function draw(){
+
+
+    if(isMousePressedbyme){
+        console.log("Hello")
+    }
 
     background(255, 255, 255)
 
@@ -106,7 +265,7 @@ function draw(){
 
 
               }
-            else if(grid[i][j].getValue()==1){
+            else if(grid[i][j].value==1){
                 
                // 52, 73, 94
                noStroke()
@@ -116,7 +275,7 @@ function draw(){
                 
 
             }
-            else if(grid[i][j].getValue()==2){
+            else if(grid[i][j].value==2){
                 
                // 52, 73, 94
 
@@ -130,11 +289,21 @@ function draw(){
 
 
                 
-              
+            // 64,224,208
+            // 148,0,211
+           
 
                stroke(0)
-               fill(0,206,209)
+            //    fill(0,206,209)
+            //    fill(r,g,b)
+            //    fill(148,0,211)
+            //    fill(grid[i][j].r,grid[i][j].g,grid[i][j].b)
+               fill(grid[i][j].rgba)
+
+
                rect(x,y,resolution,resolution)
+
+
                
 
                 
@@ -164,68 +333,163 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-async function dfs(r,c,grid){
 
-    if(r<0 || c<0 || r>rows-1 || c>cols-1 || grid[r][c].visited==true || found==true || grid[r][c].value==1){
+
+async function rgbChange(){
+    console.log("In function")
+    // 148,0,211
+   
+
+        console.log("In set function")
+        
+
+        for(let i=0;i<rows;i++){
+            for(let j=0;j<cols;j++){
+                console.log("In for loop")
+                await sleep(1)
+                if(grid[i][j].value==2){
+                    console.log("found")
+
+                    grid[i][j].changeRGB(148,0,211)
+
+                }
+            }
+        }
+
+
+
+}
+
+async function dfs(r,c,grid,val=1,path){
+   
+    if(r<0 || c<0 || r>rows-1 || c>cols-1 || grid[r][c].visited==true || found.isFound(val)==true || grid[r][c].value==1){
         return
 
     }
 
     if(grid[r][c].endPoint==true){
 
-        found=true
+        found.setFound(val)
+       
+        paths.push(path.copy())
         return
     }
 
     
 
     grid[r][c].visited=true
+    path.append([r,c])
+
+    
 
 
 
 
 
     await sleep(1)
-    console.log("Hello")
+   
 
     grid[r][c].value=2
-    // grid[r][c].setDelay(2)
 
+    await  dfs(r+1,c,grid,1,path)
+    await dfs(r-1,c,grid,1,path)
+    await dfs(r,c+1,grid,1,path)
+    await dfs(r,c-1,grid,1,path)
 
+    // grid[r][c].visited=false
+    // path.remove([r,c])
+    // grid[r][c].value=0
 
-    // setTimeout(()=>{
-    //     dfs(r+1,c,grid)
-
-    // },500)
-
-    // setTimeout(()=>{
-    //     dfs(r-1,c,grid)
-        
-
-    // },500)
     
-    // setTimeout(()=>{
-    //     dfs(r,c+1,grid)
-        
 
-    // },500)
-    // setTimeout(()=>{
-    //     dfs(r,c-1,grid)
-        
 
-    // },500)
-   await  dfs(r+1,c,grid)
-    await dfs(r-1,c,grid)
+}
+async function dfsForMaze(r,c,grid,val=1,path){
+   
+    if(r<0 || c<0 || r>rows-1 || c>cols-1 || grid[r][c].visited==true || found.isFound(val)==true || grid[r][c].value==1){
+        return
+
+    }
+
+    if(grid[r][c].endPoint==true){
+
+        // found.setFound(val)
+        console.log(path.copy())
+        paths.push(path.copy())
+        return
+    }
+
     
-    await dfs(r,c+1,grid)
-    await dfs(r,c-1,grid)
+
+    grid[r][c].visited=true
+    path.append([r,c])
+
+    
+
+
+
+
+
+    await sleep(1)
+    console.log([r,c])
+
+    grid[r][c].value=2
+
+    await  dfs(r+1,c,grid,1,path)
+    await dfs(r-1,c,grid,1,path)
+    await dfs(r,c+1,grid,1,path)
+    await dfs(r,c-1,grid,1,path)
+
+    grid[r][c].visited=false
+    path.remove([r,c])
+    grid[r][c].value=0
+
     
 
 
 }
 
 
-function BFS(r,c,grid){
+
+
+async function dfs2(r,c,grid,val,k){
+
+    if(r<0 || c<0 || r>rows-1 || c>cols-1 || grid[r][c].isVisited(val)==true || found.isFound(val)==true || grid[r][c].value==1){
+        return
+
+    }
+
+    if(grid[r][c].endPoint==true){
+
+        found.setFound(val)
+        return
+    }
+
+    
+
+    grid[r][c].setVisited(val)
+
+
+
+
+
+    await sleep(1)
+    
+
+    grid[r][c].rgba=rgba[k]
+
+   await  dfs2(r+1,c,grid,val,k)
+    await dfs2(r-1,c,grid,val,k)
+    
+    await dfs2(r,c+1,grid,val,k)
+    await dfs2(r,c-1,grid,val,k)
+    
+
+
+}
+
+
+async function BFS(r,c,grid){
 
     const rows=grid.length
     const cols=grid[0].length
@@ -243,6 +507,8 @@ function BFS(r,c,grid){
         for(let i=0;i<queue.returnValue().length;i++){
             const [r,c]=queue.popleft()
             if (grid[r][c].endPoint==true){
+                // rgbChange(grid)
+                // console.log(grid)
                 return
 
             }
@@ -254,6 +520,8 @@ function BFS(r,c,grid){
 
                 }
 
+                await sleep(1)
+
                 queue.append([r+dr,c+dc])
                 grid[r+dr][c+dc].visited=true
                 grid[r+dr][c+dc].value=2
@@ -263,12 +531,59 @@ function BFS(r,c,grid){
 
         }
 
-        
+    
+
+    }
 
 
 
+}
+
+async function BFS2(r,c,grid,k,val){
+    console.log("Hello")
+    
+
+    const rows=grid.length
+    const cols=grid[0].length
 
 
+    let queue=new Deque()
+
+    queue.append([r,c])
+
+    grid[r][c].setVisited(val)
+    
+
+    while(queue.returnValue().length>0){
+
+        for(let i=0;i<queue.returnValue().length;i++){
+            const [r,c]=queue.popleft()
+            if (grid[r][c].endPoint==true){
+                // rgbChange(grid)
+                // console.log(grid)
+                return
+
+            }
+            let neighbors=[[0,1],[0,-1],[1,0],[-1,0]]
+
+            for(let [dr,dc] of neighbors){
+                if(r+dr<0 || c+dc<0 || r+dr >rows-1 || c+dc > cols-1 || grid[r+dr][c+dc].isVisited(val)==true ||grid[r+dr][c+dc].value==1){
+                    continue
+
+                }
+
+                await sleep(1)
+
+                queue.append([r+dr,c+dc])
+                grid[r+dr][c+dc].setVisited(val)
+                grid[r+dr][c+dc].rgba=rgba[k]
+                
+
+            }
+
+        }
+
+    
 
     }
 
@@ -282,78 +597,13 @@ function BFS(r,c,grid){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function windowResized() {
-    resizeCanvas(windowWidth-20, windowHeight-200);
-    
-    
-  }
-
-function mouseClicked(){
-
-    for(let i=0;i<grid.length;i++){
-        for(let j=0;j<grid[0].length;j++){
-
-            grid[i][j].mouseClicked(mouseX,mouseY)
-
-
-
-
-        }}
-
-}
-
-function mouseDragged(){
-    for(let i=0;i<grid.length;i++){
-        for(let j=0;j<grid[0].length;j++){
-
-            grid[i][j].mouseClicked(mouseX,mouseY)
-
-
-
-
-        }}
-
-}
-
 function make2dArray(rows,cols){
     let array2=[]
 
     for(let i=0;i<rows;i++){
         let tempArr=[]
         for(let j=0;j<cols;j++){
-            tempArr.push(new Matrix(0))
+            tempArr.push(new Matrix(0,i,j))
 
         }
         array2.push(tempArr)
@@ -364,13 +614,27 @@ function make2dArray(rows,cols){
 }
 
 class Matrix{
-    constructor(value){
+    constructor(value,row,col){
         this.value=value
         this.posX=null
         this.posY=null
         this.strPoint=false
         this.endPoint=false
         this.visited=false
+        // this.r=0
+        // this.g=206
+        // this.b=209
+        this.r=52
+        this.g=73
+        this.b=94
+        this.rgb="rgb(0,206,209)"
+        // this.rgba="rgba(0,206,209,1)"
+        this.rgba=rgba[0]
+        this.visited2=false
+        this.visited3=false
+        this.visited4=false
+        this.row=row
+        this.col=col
     }
 
     getValue(){
@@ -379,6 +643,38 @@ class Matrix{
     setValue(x){
         this.value=x
     }
+    isVisited(val){
+        if(val==2){
+            return this.visited2
+
+        }
+        else if(val==3){
+            return this.visited3
+
+        }
+        else if(val==4){
+            return this.visited4
+
+        }
+
+
+    }
+    setVisited(val){
+        if(val==2){
+           this.visited2=true
+
+        }
+        else if(val==3){
+           this.visited3=true
+
+        }
+        else if(val==4){
+           this.visited4=true
+
+        }
+
+
+    }
     setDelay(x){
        
       
@@ -386,6 +682,11 @@ class Matrix{
             this.value=x
 
         },this.posX*100)
+    }
+    changeRGB(r,g,b){
+        this.r=r
+        this.g=g
+        this.b=b
     }
     setPos(x,y){
         this.posX=x
@@ -409,7 +710,85 @@ class Matrix{
       if(mx>topX && my >topY && mx< bottomX && my < bottomY && !this.endPoint && !this.strPoint){
         
         this.value==1 ? this.value=0 : this.value=1
-        console.log(`${this.value}`)
+  
+      }
+
+
+    }
+    mouseClicked2(mx,my){
+      let  topX=this.posX
+      let topY=this.posY
+
+      let bottomX=this.posX+resolution
+
+      let bottomY=this.posY+resolution
+
+      if(mx>topX && my >topY && mx< bottomX && my < bottomY ){
+
+        return this.strPoint
+
+
+        
+     
+      }
+
+
+    }
+    mouseClicked4(mx,my){
+      let  topX=this.posX
+      let topY=this.posY
+
+      let bottomX=this.posX+resolution
+
+      let bottomY=this.posY+resolution
+
+      if(mx>topX && my >topY && mx< bottomX && my < bottomY ){
+
+        return this.endPoint
+
+
+        
+     
+      }
+
+
+    }
+    mouseClicked3(mx,my){
+      let  topX=this.posX
+      let topY=this.posY
+
+      let bottomX=this.posX+resolution
+
+      let bottomY=this.posY+resolution
+
+      if(mx>topX && my >topY && mx< bottomX && my < bottomY ){
+
+        this.strPoint=true
+        strPoint=[this.row,this.col]
+
+
+        
+     
+      }
+
+
+    }
+    mouseClicked5(mx,my){
+      let  topX=this.posX
+      let topY=this.posY
+
+      let bottomX=this.posX+resolution
+
+      let bottomY=this.posY+resolution
+
+      if(mx>topX && my >topY && mx< bottomX && my < bottomY ){
+
+        this.endPoint=true
+        endPoint =[this.row,this.col]
+
+
+        
+     
       }
 
 
@@ -446,6 +825,206 @@ class Deque{
 }
 
 
+
+class Found{
+    constructor(){
+        this.found=false
+        this.found2=false
+        this.found3=false
+        this.found4=false
+
+    }
+    isFound(val){
+        if(val==1){
+            return this.found
+
+        }
+        if(val==2){
+            return this.found2
+
+        }
+        else if(val==3){
+            return this.found3
+
+        }
+        else if(val==4){
+            return this.found4
+
+        }
+
+
+    }
+    setFound(val){
+        if(val==1){
+           this.found=true
+
+        }
+        if(val==2){
+           this.found2=true
+
+        }
+        else if(val==3){
+           this.found3=true
+
+        }
+        else if(val==4){
+           this.found4=true
+
+        }
+
+
+    }
+}
+
+class Path{
+    constructor(){
+        this.value=[]
+    }
+
+    append(val){
+        this.value.push(val)
+        return this.value
+    }
+    remove(val){
+        for(let i=0;i<this.value.length;i++){
+
+            const [a,b]=this.value[i]
+
+            if(a==val[0] && b==val[1]){
+                this.value.splice(i,1)
+            }
+        }
+    }
+    copy(){
+        return [...this.value]
+    }
+}
+
+
+//As there is no built in Heap or priority queue in JavaScript This is a small attempt to Implement Heap for JavaScript and That's all folks 
+
+
+class Heap{
+    constructor(){
+        this.heap=[0]
+    }
+
+    heappush(val){
+        this.heap.push(val)
+
+        let i=this.heap.length-1
+
+        while (this.heap[i] < this.heap[Math.floor(i/2)]){
+            let temp=this.heap[i]
+
+            this.heap[i]=this.heap[Math.floor(i/2)]
+            this.heap[Math.floor(i/2)]=temp
+
+            i=Math.floor(i/2)
+
+
+        }       
+
+    }
+
+
+    heappop(){
+
+        if(this.heap.length<2){
+            return null
+        }
+
+        if(this.heap.length==2){
+            return this.heap.pop()
+        }
+       
+
+        let res=this.heap[1]
+
+        this.heap[1]=this.heap.pop()
+        let i=1
+        while (2*i < this.heap.length){
+            if(((2*i)+1<this.heap.length) && (this.heap[2*i+1] < this.heap[2*i]) &&(this.heap[i] > this.heap[(2*i)+1])){
+                
+                let temp= this.heap[i]
+                this.heap[i]= this.heap[(2*i)+1]
+                this.heap[(2*i)+1]=temp
+
+                i=2*i+1
+
+
+            }
+            else if(  this.heap[(2*i)+1] > this.heap[2*i]  ){
+               
+                let temp= this.heap[i]
+                this.heap[i]=this.heap[2*i]
+                this.heap[2*i]=temp
+
+                i=2*i
+            }
+            else{
+            
+                break
+            }
+            
+
+        
+        
+       
+
+
+        }
+     return res
+
+
+    }
+
+
+    heapify(arr){
+
+        arr.push(arr[0])
+
+        this.heap=arr
+
+        let curr=Math.floor((this.heap.length-1)/2)
+
+        while(curr>0){
+            i=curr
+            while (2*i < this.heap.length){
+                if(((2*i)+1<this.heap.length) && (this.heap[2*i+1] < this.heap[2*i]) &&(this.heap[i] > this.heap[(2*i)+1])){
+                    
+                    let temp= this.heap[i]
+                    this.heap[i]= this.heap[(2*i)+1]
+                    this.heap[(2*i)+1]=temp
+    
+                    i=2*i+1
+    
+    
+                }
+                else if(  this.heap[(2*i)+1] > this.heap[2*i]  ){
+                   
+                    let temp= this.heap[i]
+                    this.heap[i]=this.heap[2*i]
+                    this.heap[2*i]=temp
+    
+                    i=2*i
+                }
+                else{
+                
+                    break
+                }
+           
+    
+    
+            }
+            curr-=1
+        
+
+        }
+
+    }
+
+}
 
 
 
@@ -612,27 +1191,58 @@ class Deque{
 
 
 
+// let snake;
+
+// function setup(){
+//     createCanvas(600,700)
+//     snake=new Snake(0,0)
+//     frameRate(15)
+    
+// }
+// function draw(){
+//     background(0)
+//     snake.update()
+//     snake.show()
+//     function keyPressed(){
+    
+//         if(keyCode==DOWN_ARROW){
+//             snake.dir(0,-1)
+            
+//         }
+    
+//     }
 
 
 
+// }
 
 
 
+// class Snake{
+//     constructor(x,y){
+//         this.x=x
+//         this.y=y
+//         this.scale=10
+//         this.up=false
+//         this.down=false
+//         this.
+
+//     }
+//     dir(dx,dy){
+//         this.x=this.x+dx
+//         this.y=this.y-dy
+//     }
+
+//     update(){
+
+//     }
+
+//     show(){
+//         fill(200)
+//         noStroke()
+//         rect(this.x,this.y,this.scale,this.scale)
+//     }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// }
